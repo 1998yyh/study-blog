9931193990ea7c99c3ce99c3fa08e243da5a2313
yanghao <532438158@qq.com>
2022-01-27 14:04:08 +0800

    【其他修改】评论提交

--- js/index.js
+++ js/index.js
@@ -1,7 +1,7 @@
 const gitalk = new Gitalk({
   clientID: 'f087538d5f06a045eec0',
   clientSecret: 'a3bca129f513da4e017367469fe6740f81ea9cf7',
-  repo: 'study-blog-gitalk',
+  repo: '1998yyh.github.io',
   owner: '1998yyh',
   admin: ['1998yyh'],
   // facebook-like distraction free mode
