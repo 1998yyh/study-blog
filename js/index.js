95411dbc868ffc20023a5b9d44621e6375bac2aa
yanghao <532438158@qq.com>
2022-01-27 11:47:59 +0800

    【其他修改】测试评论

--- js/index.js
+++ js/index.js
@@ -1,9 +1,9 @@
 const gitalk = new Gitalk({
   clientID: 'f087538d5f06a045eec0',
   clientSecret: 'a3bca129f513da4e017367469fe6740f81ea9cf7',
-  repo: 'Github repo',
-  owner: 'Github repo owner',
-  admin: ['Github repo collaborators, only these guys can initialize github issues'],
+  repo: 'study-blog-gitalk',
+  owner: '1998yyh',
+  admin: ['1998yyh'],
   // facebook-like distraction free mode
   distractionFreeMode: false
 })
