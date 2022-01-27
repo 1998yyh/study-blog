db920dcfb2ebf990ebcbbb0f9739c8f576b03f8e
yanghao <532438158@qq.com>
2022-01-27 14:22:51 +0800

    【其他修改】label缩短

--- js/index.js
+++ js/index.js
@@ -1,9 +1,10 @@
 const gitalk = new Gitalk({
   clientID: 'f087538d5f06a045eec0',
   clientSecret: 'a3bca129f513da4e017367469fe6740f81ea9cf7',
-  repo: '1998yyh.github.io',
+  repo: 'study-blog-gitalk',
   owner: '1998yyh',
   admin: ['1998yyh'],
   // facebook-like distraction free mode
-  distractionFreeMode: false
+  distractionFreeMode: false,
+  id:location.pathname
 })
