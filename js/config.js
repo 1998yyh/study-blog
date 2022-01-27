8b7b91fe4aa8a8acb6f760b878909cdca2ac4b7d
yanghao <532438158@qq.com>
2022-01-27 11:44:39 +0800

    【新增】测试评论

---
+++ js/config.js
@@ -0,0 +1,10 @@
+window.$docsify = {
+  name: '',
+  nameLink: '', // 链接到的地址
+  logo: '',
+  repo: '',
+  auto2top: true, // 更改线路自动滚动到顶部
+  loadSidebar: true,
+  loadNavbar: true,
+  coverpage: true,
+}
--- js/index.js
+++ js/index.js
@@ -1,10 +1,9 @@
-window.$docsify = {
-  name: '',
-  nameLink: '', // 链接到的地址
-  logo: '',
-  repo: '',
-  auto2top: true, // 更改线路自动滚动到顶部
-  loadSidebar: true,
-  loadNavbar: true,
-  coverpage: true,
-}
+const gitalk = new Gitalk({
+  clientID: 'f087538d5f06a045eec0',
+  clientSecret: 'a3bca129f513da4e017367469fe6740f81ea9cf7',
+  repo: 'Github repo',
+  owner: 'Github repo owner',
+  admin: ['Github repo collaborators, only these guys can initialize github issues'],
+  // facebook-like distraction free mode
+  distractionFreeMode: false
+})
